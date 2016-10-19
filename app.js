c7c57d78f1e71ee7ec0083fe62970bd43ec21622
Yarith Joseph <yarithjoseph@gmail.com>
2016-10-19 14:40:20 +0200

    it now prints out the index on a new page... not exactly what it's supposed to do eh

--- app.js
+++ app.js
@@ -17,10 +17,33 @@ app.get ("/form", (request, response) =>{
 })
     
 app.post('/form', (request,response) => {
-    // console.log(request.body)
-    response.render(request.body)
+    console.log(request.body )
+    
+    fs.readFile(__dirname + "/users.json", (error, data) => {
+		if (error) throw error
+			// error ? throw error
+		let parseData = JSON.parse(data)
+		response.render("result", {data:parseData} )
+	})
+    
 
 })
+
+// const first = request.body.firstname;
+    // const last = request.body.lastname;
+    // const mail = request.body.email;
+
+// Maybe use response.send
+
+ // json.users.push = username;
+ //    fs.writeFile("./storage.json", JSON.stringify(json, null, 4) , 'utf-8');
+ //    delete require.cache[require.resolve('./storage.json')];
+ //    json = require("./storage.json");
+
+
+// app.post('/game', function (req, res) {
+//     res.render('some-file', { name: req.body.name });
+// });
    
 
 //this is the part that will render the index page when you type /index after the localhost
